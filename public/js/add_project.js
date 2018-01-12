aa56f210765a5f49ab50047dd49984f5531a0427
Mark <mark.onyango@gmail.com>
2018-01-12 07:09:25 +0100

    .

--- public/js/add_project.js
+++ public/js/add_project.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     $('#type').on('change', function(event){
         const selected = $('#type option:selected').val();
+        if(selected === 'dea'){
+            $('#options_dea').show();
+        } else {
+            $('#options_dea').hide();
+        }
     });
 });
