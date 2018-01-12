036eac5cdb8be42c1e2f74eeca1a269bb28a6fac
Mark <mark.onyango@gmail.com>
2018-01-12 17:44:02 +0100

    Project addition form. Send FormData objects with jQuery AJAX.

--- public/js/add_project.js
+++ public/js/add_project.js
@@ -1,10 +1,54 @@
 $(document).ready(function() {
+
+    // Form evaluation stuff
     $('#type').on('change', function(event){
         const selected = $('#type option:selected').val();
         if(selected === 'dea'){
-            $('#options_dea').show();
+            $('#options_dea').show('fast','swing');
+            $('input[name="qa_files"]').hide();
         } else {
             $('#options_dea').hide();
+            $('input[name="qa_files"]').show('fast','swing');
+            
         }
     });
+
+
+    // Form submission handling
+    $('#add_project_form').on('submit', function(event) {
+        event.preventDefault();
+        const form = $(this);
+        var formData = false;
+
+        if (window.FormData) {
+            formData = new FormData(form[0]);
+            console.log("FormData supported")
+        }
+        
+        
+        $.ajax({
+            type:   'POST',
+            url:    '/projects/upload',
+            data:   formData ? formData : form.serialize,
+            processData:    false,
+            contentType:    false,
+
+            beforeSend: function() {
+                console.log("Starting to upload files");
+            },
+            complete: function() {
+                console.log("Completed ajax request");
+            },
+            success: function(res) {
+                console.log("Ajax request was successful",res);
+                $('#exampleModal').modal('hide');
+            },
+            fail: function() {
+                console.log("Ajax request failed");
+            },
+            error: function() {
+                console.log("Ajax request returned with an error");
+            }
+        });
+    })
 });
--- routes/projects.js
+++ routes/projects.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 const Project = require('../server/schema/project');
+
+
+// Where do the file uploads go to
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, '[' + Date.now() + ']-' + file.originalname)
+    }
+});
+const upload = multer({ storage: storage });
+
 
 router.get('/', function (req, res) {
     Project.find({}, function (error, projects) {
@@ -11,6 +25,13 @@ router.get('/', function (req, res) {
             res.render('projects', { title: 'Projects', projects: projects, script: 'js/add_project.js' });
         }
     });
+});
+
+
+router.post('/upload', upload.array('qa_files'), function(req, res) {
+    // console.log(req.body, req.files, req.file);
+    // req.flash('success_msg',req.body);
+    res.status(200).json(req.body);
 });
 
 module.exports = router;
