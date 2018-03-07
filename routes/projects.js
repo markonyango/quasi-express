fa258d4f27205412f7fb9f182ff026382858122e
Mark <mark.onyango@gmail.com>
2018-03-07 17:57:31 +0100

    Projects log- and errorfile are now being presented on the front-end

--- routes/projects.js
+++ routes/projects.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-const { alignReferenceFolder, uploadPath } = require('../settings');
-const fs = require('fs-extra');
-const path = require('path');
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const { alignReferenceFolder, uploadPath } = require('../settings')
+const fs = require('fs-extra')
+const path = require('path')
 const printOut = require('../printOut')
 
-const Project = require('../server/schema/ProjectSchema');
-const User = require('../server/schema/UserSchema');
+const Project = require('../server/schema/ProjectSchema')
+const User = require('../server/schema/UserSchema')
 
 
 // Where do the file uploads go to
@@ -17,21 +17,21 @@ var storage = multer.diskStorage({
     cb(null, savePath)
   },
   filename: function (req, file, cb) {
-    let uid = req.user._id;
+    let uid = req.user._id
 
     cb(null, file.originalname)
   }
-});
-const upload = multer({ storage: storage });
+})
+const upload = multer({ storage: storage })
 
 
 router.get('/', function (req, res) {
-  let uid = req.user._id;
+  let uid = req.user._id
 
   if (req.query.json === 'true') {
     Project.find({ uid: uid }).exec()
       .then(projects => {
-        res.status(200).json(projects);
+        res.status(200).json(projects)
       })
       .catch(error => {
         console.error(`Could not fetch the list of projects for ${uid}: ${error}`)
@@ -40,28 +40,28 @@ router.get('/', function (req, res) {
   } else {
     Project.find({ uid: uid }).exec()
       .then(projects => {
-        res.render('projects', { title: 'Projects', projects: projects, script: 'js/projects.js' });
+        res.render('projects', { title: 'Projects', projects: projects, script: 'js/projects.js' })
       })
       .catch(error => {
-        req.flash('error_msg', 'Something went wrong while getting the list of projects: ' + error);
-        res.redirect('/projects');
+        req.flash('error_msg', 'Something went wrong while getting the list of projects: ' + error)
+        res.redirect('/projects')
       })
   }
-});
+})
 
 
 router.post('/upload', upload.array('files'), function (req, res, next) {
 
-  const files = req.files;
-  const projectname = req.body.projectname;
-  const projecttype = req.body.projecttype;
-  const settings = req.body.settings || {};
-  const status = 'queued';
-  const uid = req.user._id;
+  const files = req.files
+  const projectname = req.body.projectname
+  const projecttype = req.body.projecttype
+  const settings = req.body.settings || {}
+  const status = 'queued'
+  const uid = req.user._id
 
   if (!files || !projectname || !projecttype || !uid) {
     console.error('Invalid POST request to project upload route!')
-    res.status(400).json('Invalid POST request!');
+    res.status(400).json('Invalid POST request!')
     next()
   }
 
@@ -73,40 +73,40 @@ router.post('/upload', upload.array('files'), function (req, res, next) {
     files: [],
     uid: uid,
     created: Date.now()
-  });
+  })
 
 
   for (let file of files) {
-    project.files.push(file.filename);
+    project.files.push(file.filename)
   }
 
   project.save()
     .then(result => res.status(200).json(result))
     .catch(error => {
       console.error(`Something went wrong while saving project ${project._id}: ${error}`)
-      res.status(500).json(error);
+      res.status(500).json(error)
     })
-});
+})
 
 router.get('/references', function (req, res) {
   fs.readdir(alignReferenceFolder, (err, files) => {
     if (err) {
-      res.status(500).json(err);
+      res.status(500).json(err)
     } else {
       files = files.filter(file => {
         return file.indexOf('.fasta') >= 0 ? true : false
-      });
-      res.status(200).json(files);
+      })
+      res.status(200).json(files)
     }
-  });
-});
+  })
+})
 
 
 router.get('/:id', function (req, res) {
 
   // View project details
-  const id = req.params.id;
-  const uid = req.user._id;
+  const id = req.params.id
+  const uid = req.user._id
 
   if (req.query.json === 'true') {
     Project.findOne({ _id: id, uid: uid }).exec()
@@ -118,25 +118,31 @@ router.get('/:id', function (req, res) {
     Project.findOne({ _id: id, uid: uid }).exec()
       .then(project => {
         if (project.status === 'done') {
-          let files = fs.readdirSync(project.savePath);
-          files = files.filter(file => (file.indexOf('log') >= 0 || file.indexOf('error') >= 0) ? true : false);
-          res.render('project', { title: 'Project', project: project, log: files });
+          let errorFile = fs.readFile(path.join(project.savePath, 'error.txt'), 'utf8')
+          let logFile = fs.readFile(path.join(project.savePath, 'logfile.txt'), 'utf8')
+
+          Promise.all([errorFile, logFile])
+            .then(([error, log]) => {
+              res.render('project', { title: 'Project', project: project, logfile: log, errorfile: error })
+            })
+            .catch(error => console.error(`${printOut(__filename)} Could not read the logfiles for project ${project._id}: ${error}`.red))
+          
         } else {
-          res.render('project', { title: 'Project', project: project });
+          res.render('project', { title: 'Project', project: project })
         }
       })
       .catch(error => {
-        console.error(`${printOut(__filename)} Could not fetch the stats for project ${id}: ${error}`)
-        req.flash('error', error.message);
-        res.redirect(500, '/projects');
+        console.error(`${printOut(__filename)} Could not fetch the stats for project ${id}: ${error}`.red)
+        req.flash('error', error.message)
+        res.redirect(500, '/projects')
       })
   }
 
-});
+})
 
 router.put('/:id/:action', function (req, res) {
 
-  const uid = req.user._id;
+  const uid = req.user._id
   const { id, action } = req.params
 
   Project.findOne({ _id: id, uid: uid }).exec()
@@ -146,26 +152,26 @@ router.put('/:id/:action', function (req, res) {
           project.startjob()
             .then(project => res.status(200).json(project))
             .catch(error => res.status(500).json(error))
-          break;
+          break
         case 'stop':
           project.stopjob()
             .then(project => res.status(200).json(project))
             .catch(error => res.status(500).json(error))
-          break;
+          break
         case 'remove':
           project.removejob()
             .then(project => res.status(200).json(project))
             .catch(error => res.status(500).json(error))
-          break;
+          break
         default:
-          res.json(400, 'Could not identify the requested change action: ' + action);
+          res.json(400, 'Could not identify the requested change action: ' + action)
       }
     })
     .catch(error => {
       console.error(`${printOut(__filename)} There was a mistake with your PUT request to start project ${id}: ${error}`.red)
-      res.status(500).json(error);
+      res.status(500).json(error)
     })
-});
+})
 
 
-module.exports = router;
+module.exports = router
