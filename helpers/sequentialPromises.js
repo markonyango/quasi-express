f5ff7c80c9885b6fca6122d340a29eb786efaa73
Mark <>
2018-03-16 13:58:13 +0100

    (Addition) sequentialPromises.js to execute Promisechains with array.reduce

---
+++ helpers/sequentialPromises.js
@@ -0,0 +1,22 @@
+/**
+ * Runs Promises from the input array in a chained manner.
+ * This code was taken from: 
+ * https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+ * and modified to return all Promise results instead of just the last one.
+ *
+ * @param {array} promiseArray - Array of Promises
+ * @return {Promise} Promise of Array with resolved Promises
+ */
+module.exports = function (promiseArray) {
+    let array = []
+    return promiseArray.reduce((promiseChain, currentPromise) => {
+        return promiseChain.then(() => {
+            return currentPromise()
+                .then((res) => {
+                    array.push(res)
+                    return res
+                })
+        })
+    }, Promise.resolve())
+        .then(() => { return array })
+}
