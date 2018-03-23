8947466a5dd3a1443466f2e3a1ace716e5f06297
Mark <mark.onyango@gmail.com>
2018-03-23 22:05:56 +0100

    (QAReport) Each QA subreport is presented in its own DIV (QAReport) Each subreport div is a bootstrap carousel (1 slide/file)

--- public/js/results/qa.js
+++ public/js/results/qa.js
@@ -1,8 +1,15 @@
-let divResults = document.getElementById('results')
+let resultDiv = document.getElementById('results')
 
 function makeCanvas(parentDiv) {
+  let canvasDiv = document.createElement('div')
   let canvas = document.createElement('canvas')
-  parentDiv.appendChild(canvas)
+
+  canvasDiv.classList.add('carousel-item')
+  canvas.classList.add('d-block', 'w-100')
+
+  canvasDiv.appendChild(canvas)
+  parentDiv.appendChild(canvasDiv)
+
   return canvas
 }
 
@@ -25,73 +32,178 @@ function makeChart(canvas, type) {
   })
 }
 
-// Create the base distribution chart
+function makeDiv(id, classes = []) {
+  let div = document.createElement('div')
+  div.id = id
+  div.classList.add(...classes)
+  div.classList.add('carousel', 'slide')
+  let carouselInner = document.createElement('div')
+  carouselInner.classList.add('carousel-inner')
+  div.appendChild(carouselInner)
+
+  // Create carousel controls
+  // Left arrow button
+  let leftArrow = document.createElement('a')
+  leftArrow.classList.add('carousel-control-prev')
+  leftArrow.setAttribute('role', 'button')
+  leftArrow.setAttribute('data-slide', 'prev')
+  leftArrow.setAttribute('href', '#' + id)
+  leftArrow.innerHTML = `<span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span>`
+
+  // Right arrow button
+  let rightArrow = document.createElement('a')
+  rightArrow.classList.add('carousel-control-next')
+  rightArrow.setAttribute('role', 'button')
+  rightArrow.setAttribute('data-slide', 'next')
+  rightArrow.setAttribute('href', '#' + id)
+  rightArrow.innerHTML = `<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span>`
+
+  // Append both arrow buttons to the div element
+  div.appendChild(leftArrow)
+  div.appendChild(rightArrow)
+
+  resultDiv.appendChild(div)
+  return carouselInner
+}
+
+// Create the distribution charts
 fetch(window.location.pathname + '?json=true', {
   credentials: 'include'
 })
   .then(res => res.json())
   .then(({ QAReport }) => {
     let { baseDistribution, lengthDistribution, phredDistribution } = QAReport
-
     let maxReadLength = lengthDistribution.reduce(
       (max, file) => (file.data.length > max ? file.data.length : max),
       0
     )
 
-    let resultDiv = document.getElementById('results')
+    createBaseDistributionGraphs(baseDistribution, maxReadLength)
+    createLengthDistributionGraphs(lengthDistribution, maxReadLength)
+    createPhredDistributionGraphs(phredDistribution, maxReadLength)
+  })
 
-    // Creating base distribution charts
-    let baseDiv = document.createElement('div')
-    baseDiv.id = 'baseDiv'
-    resultDiv.appendChild(baseDiv)
-    for (let file of baseDistribution) {
-      let canvas = makeCanvas(baseDiv)
-      let chart = makeChart(canvas, 'line')
-      chart.data.labels = Array(maxReadLength)
-        .fill(0)
-        .map((value, index) => index + 1)
-      chart.options.layout.padding = { left: 10, right: 30 }
-      chart.options.scales.xAxes[0] = {
-        ticks: {
-          callback: function(value, index, values) {
-            let retVal = (x, gap) => (value % gap === 0 ? value : null)
-            if (values.length > 100) return retVal(value, 10)
-            else if (values.length > 50) return retVal(value, 5)
-            else if (values.length > 20) return retVal(value, 2)
-            else return value
-          }
+function createBaseDistributionGraphs(baseDistribution, maxReadLength) {
+  let baseDiv = makeDiv('baseDiv', ['jumbotron', 'project-details'])
+  baseDistribution.map((file, index) => {
+    let canvas = makeCanvas(baseDiv)
+    let chart = makeChart(canvas, 'line')
+
+    // If it is the first canvas we need to set its class to active
+    if (index === 0) canvas.parentElement.classList.add('active')
+
+    chart.data.labels = Array(maxReadLength)
+      .fill(0)
+      .map((value, index) => index + 1)
+    chart.options.layout.padding = { left: 10, right: 30 }
+    chart.options.scales.xAxes[0] = {
+      ticks: {
+        callback: function(value, index, values) {
+          let retVal = (x, gap) => (value % gap === 0 ? value : null)
+          if (values.length > 100) return retVal(value, 10)
+          else if (values.length > 50) return retVal(value, 5)
+          else if (values.length > 20) return retVal(value, 2)
+          else return value
         }
       }
-      chart.options.title.text =
-        'Base Distributions per Cycle for ' + file.label
-      chart.options.title.display = true
-
-      chart.data.datasets.push({
-        label: 'A',
-        data: file.data.A,
-        backgroundColor: 'rgba(255, 0, 0, 0.50)'
-      })
-      chart.data.datasets.push({
-        label: 'T',
-        data: file.data.T,
-        backgroundColor: 'rgba(0, 255, 0, 0.50)'
-      })
-      chart.data.datasets.push({
-        label: 'G',
-        data: file.data.G,
-        backgroundColor: 'rgba(0, 0, 255, 0.50)'
-      })
-      chart.data.datasets.push({
-        label: 'C',
-        data: file.data.C,
-        backgroundColor: 'rgba(255, 255, 0, 0.50)'
-      })
-      chart.data.datasets.push({
-        label: 'N',
-        data: file.data.N,
-        backgroundColor: 'rgba(0, 0, 0, 0.50)'
-      })
-
-      chart.update()
     }
+    chart.options.title.text = 'Base Distributions per Cycle for ' + file.label
+    chart.options.title.display = true
+
+    chart.data.datasets = Object.entries(file.data).map(base => ({
+      label: base[0],
+      data: base[1]
+    }))
+    let colors = [
+      'rgba(255,0,0,0.75)',
+      'rgba(0,255,0,0.75)',
+      'rgba(0,0,255,0.75)',
+      'rgba(255,255,0,0.75)',
+      'rgba(0,0,0,0.75)'
+    ]
+    chart.data.datasets = chart.data.datasets.map((dataset, index) => ({
+      ...dataset,
+      backgroundColor: colors[index]
+    }))
+
+    chart.update()
   })
+}
+
+function createLengthDistributionGraphs(lengthDistribution, maxReadLength) {
+  let lengthDiv = makeDiv('lengthDiv', ['jumbotron', 'project-details'])
+  let canvas = makeCanvas(lengthDiv)
+  let chart = makeChart(canvas, 'bar')
+
+  // There will be only one canvas but we still need to set its parents class to active
+  canvas.parentElement.classList.add('active')
+
+  chart.data.labels = Array(maxReadLength)
+    .fill(0)
+    .map((value, index) => index + 1)
+  chart.options.layout.padding = { left: 10, right: 30 }
+  chart.options.scales.xAxes[0] = {
+    ticks: {
+      callback: function(value, index, values) {
+        let retVal = (x, gap) => (value % gap === 0 ? value : null)
+        if (values.length > 100) return retVal(value, 10)
+        else if (values.length > 50) return retVal(value, 5)
+        else if (values.length > 20) return retVal(value, 2)
+        else return value
+      }
+    }
+  }
+  chart.options.title.text = 'Readlength Distributions'
+  chart.options.title.display = true
+
+  lengthDistribution.map(file => {
+    chart.data.datasets.push({
+      label: file.label,
+      data: file.data,
+      backgroundColor: file.backgroundColor
+    })
+  })
+
+  chart.update()
+}
+
+function createPhredDistributionGraphs(phredDistribution, maxReadLength) {
+  let phredDiv = makeDiv('phredDiv', ['jumbotron', 'project-details'])
+
+  phredDistribution.map((file, index) => {
+    let canvas = makeCanvas(phredDiv)
+    let chart = makeChart(canvas, 'bar')
+    let maxPhredScore = Math.max(
+      ...Object.values(file.data).map(arr =>
+        arr.reduce((max, val, index) => (index > max && val !== 0 ? index : max), 0)
+      )
+    )
+
+    // If it is the first canvas we need to set its class to active
+    if (index === 0) canvas.parentElement.classList.add('active')
+
+    chart.options.title.text = 'Phred Score Distributions per base for '
+    chart.options.title.display = true
+
+    chart.data.labels = Array(maxPhredScore + 2)
+      .fill(0)
+      .map((val, index) => index)
+
+    chart.data.datasets = Object.entries(file.data).map(base => ({
+      label: base[0],
+      data: base[1]
+    }))
+    let colors = [
+      'rgba(255,0,0,0.75)',
+      'rgba(0,255,0,0.75)',
+      'rgba(0,0,255,0.75)',
+      'rgba(255,255,0,0.75)'
+    ]
+    chart.data.datasets = chart.data.datasets.map((dataset, index) => ({
+      ...dataset,
+      backgroundColor: colors[index]
+    }))
+
+    chart.update()
+  })
+}
--- server/schema/utils/QAReport.js
+++ server/schema/utils/QAReport.js
@@ -2,90 +2,105 @@ const fs = require('fs-extra')
 const path = require('path')
 
 function QAReport(projectDocument) {
-    this.files = projectDocument.files
-    this.savePath = projectDocument.savePath
-    this.maxLength = 0
+  this.files = projectDocument.files
+  this.savePath = projectDocument.savePath
+  this.maxLength = 0
 
-    this.lengthDistribution = null
-    this.phredDistribution = null
-    this.baseDistribution = null
+  this.lengthDistribution = null
+  this.phredDistribution = null
+  this.baseDistribution = null
 }
 
-QAReport.prototype.generateReport = function () {
-    // maxLength is being calculated in readLengthDistribution only
-    // so run this first
-    this.lengthDistribution = this.readLengthDistribution()
-    this.baseDistribution = this.readBaseDistribution()
-    this.phredDistribution = this.readPhredDistribution()
+QAReport.prototype.generateReport = function() {
+  // maxLength is being calculated in readLengthDistribution only
+  // so run this first
+  this.lengthDistribution = this.readLengthDistribution()
+  this.baseDistribution = this.readBaseDistribution()
+  this.phredDistribution = this.readPhredDistribution()
+
+  return {
+    lengthDistribution: this.lengthDistribution,
+    baseDistribution: this.baseDistribution,
+    phredDistribution: this.phredDistribution
+  }
+}
+
+QAReport.prototype.readLengthDistribution = function() {
+  return this.files.map(file => {
+    let buff = this.readResultFile(file, '-length_dist.txt')
+    let data = buff
+      .trim()
+      .split(' ')
+      .map(Number)
+    let label = path.basename(file)
+    this.maxLength < data.length ? (this.maxLength = data.length) : null
 
     return {
-        lengthDistribution: this.lengthDistribution,
-        baseDistribution: this.baseDistribution,
-        phredDistribution: this.phredDistribution
+      label,
+      data,
+      borderWidth: 1,
+      backgroundColor: backgroundColor()
     }
+  })
 }
 
-QAReport.prototype.readLengthDistribution = function () {
+QAReport.prototype.readPhredDistribution = function() {
+  return this.files.map(file => {
+    let buff = this.readResultFile(file, '-phred_dist.txt')
+    let data = buff
+      .trim()
+      .split(' ')
+      .map(Number)
+    let A = data.splice(0,61)
+    let T = data.splice(0,61)
+    let G = data.splice(0,61)
+    let C = data.splice(0,61)
 
-    return this.files.map(file => {
-        let buff = this.readResultFile(file, '-length_dist.txt')
-        let data = buff.trim().split(' ').map(Number)
-        let label = path.basename(file)
-        this.maxLength < data.length ? this.maxLength = data.length : null
+    data = { A, T, G, C }
 
-        return {
-            label,
-            data,
-            borderWidth: 1,
-            backgroundColor: backgroundColor()
-        }
-    })
+    let label = path.basename(file)
+
+    return {
+      label,
+      data,
+      backgroundColor: backgroundColor()
+    }
+  })
 }
 
-QAReport.prototype.readPhredDistribution = function () {
+QAReport.prototype.readBaseDistribution = function() {
+  return this.files.map(file => {
+    let buff = this.readResultFile(file, '-base_dist.txt')
+    // Turn this into a 2d array with 6 rows, representing A, T, G, C, N, GC frequencies respectively
+    // Each row has max_read_length entries
+    let data = buff
+      .trim()
+      .split('\n')
+      .map(row =>
+        row
+          .trim()
+          .split('\t')
+          .map(Number)
+      )
+    data = transpose(data)
 
-    return this.files.map(file => {
-        let buff = this.readResultFile(file, '-phred_dist.txt')
-        let data = buff.trim().split(' ').map(Number)
-            data = toMatrix(data, 4) // Turn it into a (readLength x 4) matrix
-            data = transpose(data) // Transpose so each row resembles qualities per A,T,G,C respectively
+    let [A, T, G, C, N, GC] = data
+    data = { A, T, G, C, N, GC }
 
-        let label = path.basename(file)
+    let label = path.basename(file)
 
-        return {
-            label,
-            data,
-            borderWidth: 1,
-            backgroundColor: backgroundColor()
-        }
-    })
+    return {
+      label,
+      data,
+      borderWidth: 1,
+      backgroundColor: backgroundColor()
+    }
+  })
 }
 
-QAReport.prototype.readBaseDistribution = function () {
-    return this.files.map(file => {
-        let buff = this.readResultFile(file, '-base_dist.txt')
-        // Turn this into a 2d array with 6 rows, representing A, T, G, C, N, GC frequencies respectively
-        // Each row has max_read_length entries
-        let data = buff.trim().split('\n').map(row => row.trim().split('\t').map(Number))
-            data = transpose(data)
-
-        let [A, T, G, C, N, GC] = data
-        data = { A, T, G, C, N, GC }
-
-        let label = path.basename(file)
-
-        return {
-            label,
-            data,
-            borderWidth: 1,
-            backgroundColor: backgroundColor()
-        }
-    })
-}
-
-QAReport.prototype.readResultFile = function (file, pattern) {
-    let filename = path.join(this.savePath, `${file}${pattern}`)
-    return fs.readFileSync(filename, 'utf-8')
+QAReport.prototype.readResultFile = function(file, pattern) {
+  let filename = path.join(this.savePath, `${file}${pattern}`)
+  return fs.readFileSync(filename, 'utf-8')
 }
 
 /**
@@ -95,7 +110,7 @@ QAReport.prototype.readResultFile = function (file, pattern) {
  * @returns {number} Random number between min and max
  */
 function getRandom(min, max) {
-    return Math.random() * (max - min) + min
+  return Math.random() * (max - min) + min
 }
 
 /**
@@ -103,7 +118,9 @@ function getRandom(min, max) {
  * @returns {string} rgba-function call with random values and opacity 1 as string
  */
 function backgroundColor() {
-    return `rgba(${Math.floor(getRandom(0, 255))}, ${Math.floor(getRandom(0, 255))}, ${Math.floor(getRandom(0, 255))}, 1)`
+  return `rgba(${Math.floor(getRandom(0, 255))}, ${Math.floor(getRandom(0, 255))}, ${Math.floor(
+    getRandom(0, 255)
+  )}, 1)`
 }
 
 /**
@@ -113,7 +130,7 @@ function backgroundColor() {
  * @returns {array} Transposed array
  */
 function transpose(a) {
-    return a[0].map((_, c) => a.map(r => r[c]))
+  return a[0].map((_, c) => a.map(r => r[c]))
 }
 
 /**
@@ -124,8 +141,11 @@ function transpose(a) {
  * @returns {Array.<Array.<any>>} 2d array with width columns
  */
 function toMatrix(arr, width) {
-    return arr.reduce((rows, key, index) => (index % width == 0 ? rows.push([key])
-        : rows[rows.length - 1].push(key)) && rows, [])
+  return arr.reduce(
+    (rows, key, index) =>
+      (index % width == 0 ? rows.push([key]) : rows[rows.length - 1].push(key)) && rows,
+    []
+  )
 }
 
 module.exports = QAReport
