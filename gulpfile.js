927715be7b6da5b21ca6e1cc405888d9fe3fe9df
Mark <mark.onyango@gmail.com>
2018-04-13 15:52:31 +0200

    Maintenance

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,29 @@ let autoprefixer = require('gulp-autoprefixer')
 let nodemon = require('gulp-nodemon')
 let gulp = require('gulp')
 
-gulp.task('css', function(done) {
-  gulp
+gulp.task('css', function() {
+  let stream = gulp
     .src('css/*.css')
     .pipe(cleanCSS())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('public/css'))
-  done()
+  return stream
 })
 
 gulp.task('start', function() {
-  nodemon({
+  let stream = nodemon({
     script: './bin/www',
     tasks: ['css'],
     ext: 'hbs js css',
+
     ignore: [
-      'public/css/style.min.css',
+      'public/css',
       'gulpfile.js',
-      '*.txt',
-      'uploads/*',
-      'node-modules/*'
+      'uploads',
+      '.git'
     ],
-    verbose: false
+    verbose: true
   })
+  return stream
 })
