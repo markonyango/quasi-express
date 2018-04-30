9c4b3658ead09f384d092af25b72b450e0434660
Mark <mark.onyango@gmail.com>
2018-04-30 20:11:48 +0200

    (Travis) Tweaked Mocha settings

--- test.js
+++ test.js
@@ -1,37 +1,27 @@
 const chaiHttp = require('chai-http')
-const chai, { assert, expect } = require('chai')
-const { mongoDB } = require('./settings')
-const mongoose = require('mongoose')
+const chai = require('chai')
+const { assert, expect } = require('chai')
+const server = require('./server/server')
 const app = require('./app')
 
 chai.use(chaiHttp)
 
 describe('App', function() {
-  before(function() {
-    return mongoose
-      .connect(`mongodb://${mongoDB}:27017/quasi-express`)
-      .then(
-        ({ connection }) =>
-          assert.isAbove(connection.readyState, 0) && assert.isNumber(connection.readyState)
-      )
-      .catch(error => assert.isNull(error, error.message))
-  })
-
-  it('should create an open connection to the MongoDB server', () =>
-    assert.strictEqual(mongoose.connection.readyState, 1))
+  it('should create an open connection to the MongoDB server', () => assert.strictEqual(server.connection.readyState, 1))
   it('should be able to login', () => {
-    let agent = chai.request.agent(app)
-    return agent
+    return chai
+      .request(app)
       .post('/users/login')
       .send({ email: 'mark@gmail.com', password: 'vegeta12', json: 'true' })
       .then(res => {
-        return expect(res).to.have.cookie('connect.sid')
-      })
-      .then(() => {
-        console.log('stop')
-        agent.close()
+        assert.strictEqual(res.status, 200)
+        assert.isObject(res.body)
+        assert.isNotEmpty(res.body)
+        assert.strictEqual(res.body.email, 'mark@gmail.com')
+        assert.strictEqual(res.body.role, 'User')
+        expect(res).to.have.cookie('connect.sid')
       })
   })
 
-  after(() => mongoose.disconnect().catch(error => assert.isNull(error, error.message)))
+  after(() => server.disconnect())
 })
